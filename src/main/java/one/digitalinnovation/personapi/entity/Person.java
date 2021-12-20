12236a7cd99659aced18078fb2385cb47a9f5d2f
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2021-12-20 12:08:13 -0300

    Change Person and Phone - complete annotations

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -16,22 +16,31 @@ import javax.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.List;
 
-@Entity
-@Data
+@Entity //anotação para informar que essa classe é uma entidade. Tem que colocar uma chave primária, que no caso será o id.
+@Data //com essa anotação, insere automaticamete getter e setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor //anotação para gerar construtor com argumentos
+@NoArgsConstructor //anotação para gerar construtor sem argumentos
 public class Person {
 
+    @Id //informando a chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //a geração do id será de forma automática
     private Long id;
 
+    @Column(nullable = false) //anotação para informar que o campo dessa coluna deve ser obrigatório. É not null
     private String firstName;
 
+    @Column(nullable = false) //anotação para informar que o campo dessa coluna deve ser obrigatório. É not null
     private String lastName;
 
+    @Column(nullable = false, unique = true) //unique serve para dizer que o campo é um valor único. Não pode ter CPF repetido.
     private String cpf;
 
     private LocalDate birthDate;
 
+    /* O JPA também possibilita fazer a anotação de relacionamento entre as tabelas.
+    No caso, será o relacionamento de um para muitos, com a anotação @OneToMany
+    */
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<Phone> phones;
 }
--- src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -14,17 +14,22 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
-@Data
+@Entity //anotação para informar que essa classe é uma entidade. Tem que colocar uma chave primária, que no caso será o id.
+@Data //com essa anotação, insere automaticamete getter e setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor //anotação para gerar construtor com argumentos
+@NoArgsConstructor //anotação para gerar construtor sem argumentos
 public class Phone {
 
+    @Id //informando a chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //a geração do id será de forma automática
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false) //anotação para informar que o campo dessa coluna deve ser obrigatório. É not null
     private PhoneType type;
 
+    @Column(nullable = false) ////anotação para informar que o campo dessa coluna deve ser obrigatório. É not null
     private String number;
 
 
