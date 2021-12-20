661326b221e615bad03043969db7509a5444d89f
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2021-12-20 11:32:08 -0300

    Change Person and Phone

--- src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -21,15 +21,10 @@ import javax.persistence.Id;
 @NoArgsConstructor
 public class Phone {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private PhoneType type;
 
-    @Column(nullable = false)
     private String number;
 
 
